4d573d4338d2e3885cef5410e938e7d9f59c515b
TanJunChuan <98572846+tan-j-c@users.noreply.github.com>
2023-09-10 10:27:09 +0800

    Add files via upload

---
+++ config.py
@@ -0,0 +1,7 @@
+customhost = "employee.curnhep4iwiv.us-east-1.rds.amazonaws.com"
+customuser = "aws_user"
+custompass = "Bait3273"
+customdb = "employee"
+custombucket = "tanjunchuan-employee"
+customregion = "us-east-1"
+
